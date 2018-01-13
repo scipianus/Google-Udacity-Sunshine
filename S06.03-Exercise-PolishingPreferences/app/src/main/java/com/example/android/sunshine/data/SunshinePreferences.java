8609872fd8f9a9d42d747b1778de0bffbafda4cf
scipianus <ciprian.olariu05@gmail.com>
2018-01-14 00:53:26 +0200

    Completed exercise 14

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -16,6 +16,9 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.support.v7.preference.PreferenceManager;
+
+import com.example.android.sunshine.R;
 
 public class SunshinePreferences {
 
@@ -87,22 +90,18 @@ public class SunshinePreferences {
      * "94043,USA" if SharedPreferences have not been implemented yet.
      */
     public static String getPreferredWeatherLocation(Context context) {
-        // TODO (1) Return the user's preferred location
-        /** This will be implemented in a future lesson **/
-        return getDefaultWeatherLocation();
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(context.getString(R.string.pref_location_key), getDefaultWeatherLocation());
     }
 
     /**
      * Returns true if the user has selected metric temperature display.
      *
      * @param context Context used to get the SharedPreferences
-     *
      * @return true If metric display should be used
      */
     public static boolean isMetric(Context context) {
-        // TODO (2) Return true if the user's preference for units is metric, false otherwise
-        /** This will be implemented in a future lesson **/
-        return true;
+        String unit = PreferenceManager.getDefaultSharedPreferences(context).getString(context.getString(R.string.pref_units_key), context.getString(R.string.pref_units_metric));
+        return unit.equals(context.getString(R.string.pref_units_metric));
     }
 
     /**
